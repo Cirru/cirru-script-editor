f0475ff85e57cd9680940b531ef07bd955e03562
jiyinyiyong <jiyinyiyong@gmail.com>
2012-10-29 10:28:59 +0800

    make it look more like a seajs module

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cirru-editor
+
+Editor for Cirrru project.
+
+---
+
+try to use seajs. but this is still not a good seajs module..
---
+++ tests/cirru-editor-spec.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+    var Cirru-editor = require('../src/cirru-editor');
+
+    describe('Cirru-editor', function() {
+
+        test('normal usage', function() {
+
+        });
+    });
+
+});
+
